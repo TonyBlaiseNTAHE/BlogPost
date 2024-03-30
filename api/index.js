b2ec8bda82a1d61a6c5996814c24690df22b2486
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-30 16:55:58 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import mangoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mangoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MangoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+  console.log("server is running on port 3000");
 });
-
