28988e832c1970840efd2b15f26711d74195d06c
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-03-31 10:37:24 +0200

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mangoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -15,10 +16,11 @@ mangoose
   });
 const app = express();
 
-/*app.use(express.json());*/
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
