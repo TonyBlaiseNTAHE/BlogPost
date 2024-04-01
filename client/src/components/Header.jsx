de296467b7f0f747ed570ec1c6c0700a4f9dd0e0
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-04-01 19:16:07 +0200

    updating header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,8 @@
-import { Navbar, TextInput, Button } from "flowbite-react";
+import { Avatar, Dropdown, Navbar, TextInput, Button } from "flowbite-react";
 import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 import React from "react";
 import logo from "../images/logo.png";
 
@@ -10,6 +11,7 @@ const Logo = () => (
 );
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-b-2">
       <Link to="/" className="flex items-center">
@@ -41,11 +43,33 @@ export default function Header() {
         <Button className="w-12 h-10" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientMonochrome="cyan" outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">@{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={"/dashboard?tab=profile"}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick="">Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientMonochrome="cyan" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
       </div>
     </Navbar>
   );
