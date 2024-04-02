8e0857b73c6a4aef78b511ec90111210559b527b
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-04-02 14:12:06 +0200

    completed the update

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { test } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import { updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
